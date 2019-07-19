52eb84ff9e02dd3d86b8f49f4e89a06f534947e8
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-19 17:11:47 -0300

    Adding BaseResourceModel

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,8 +1,11 @@
+import {BaseResourceModel} from '../../../shared/models/base-resource.model';
 
-export class CategoryModel {
+export class CategoryModel extends BaseResourceModel {
   constructor(
     public id: number,
     public name: string,
     public description: string
-  ) {}
+  ) {
+    super();
+  }
 }
